270b99be3d02a097de589afc904f1ef732ca0029
ayesha-Siddiquah <ayeshaleo93@gmail.com>
2024-11-26 15:21:32 -0800

    calci is perfect

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import './App.css';
+import Calculator from './Calculator'; // Import Calculator component
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator /> {/* Render Calculator component */}
+    </div>
+  );
+}
+
+export default App;
